44208aed0f4e867df318ea57bf9f02519902871b
Jonathan Reinink <jonathan@reinink.ca>
2019-09-30 18:48:32 -0400

    Formatting

--- components/MarkdownComponents.js
+++ components/MarkdownComponents.js
@@ -11,8 +11,16 @@ function kebabCase(str) {
 
 export default {
   h1: props => <h1 {...props} className="mb-12 text-4xl font-bold text-gray-700 leading-none" />,
-  h2: props => <h2 id={kebabCase(props.children)} className="mt-16 mb-4 text-2xl font-bold text-gray-700 leading-none" {...props} />,
-  h3: props => <h3 id={kebabCase(props.children)} className="mt-16 mb-4 text-xl font-bold text-gray-700 leading-none" {...props} />,
+  h2: props => (
+    <h2
+      id={kebabCase(props.children)}
+      className="mt-16 mb-4 text-2xl font-bold text-gray-700 leading-none"
+      {...props}
+    />
+  ),
+  h3: props => (
+    <h3 id={kebabCase(props.children)} className="mt-16 mb-4 text-xl font-bold text-gray-700 leading-none" {...props} />
+  ),
   p: props => <p className="my-6" {...props} />,
   ol: props => <ol className="list-decimal list-inside" {...props} />,
   ul: props => <ul className="list-disc list-inside" {...props} />,
@@ -27,7 +35,13 @@ export default {
     )
   },
   pre: ({ children }) => children,
-  code: ({ className, children }) => <Code className="my-8 p-6 leading-normal rounded" language={className.replace('language-', '')} children={children} />,
+  code: ({ className, children }) => (
+    <Code
+      className="my-8 p-6 leading-normal rounded"
+      language={className.replace('language-', '')}
+      children={children}
+    />
+  ),
   TabbedCodeExamples: props => <TabbedCodeExamples className="my-8 rounded overflow-hidden" {...props} />,
   inlineCode: props => <code className="font-mono text-sm bg-gray-200 rounded p-1 whitespace-no-wrap" {...props} />,
 }
