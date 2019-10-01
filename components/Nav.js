a21db35dfc9793ea31078072f80cf84c18d0d5ca
Jonathan Reinink <jonathan@reinink.ca>
2019-10-01 10:48:08 -0400

    Add active states to nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,110 +1,121 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-export default ({ className }) => (
-  <nav className={className}>
-    <div className="text-xs font-bold uppercase text-gray-500 tracking-widest">Getting started</div>
-    <ul>
-      <li className="mt-4">
-        <Link href="/">
-          <a className="hover:underline font-medium text-gray-700">Introduction</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/how-it-works">
-          <a className="hover:underline font-medium text-gray-700">How it works</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/installation">
-          <a className="hover:underline font-medium text-gray-700">Installation</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/server-side-setup">
-          <a className="hover:underline font-medium text-gray-700">Server-side setup</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/client-side-setup">
-          <a className="hover:underline font-medium text-gray-700">Client-side setup</a>
-        </Link>
-      </li>
-    </ul>
-    <div className="mt-12 text-xs font-bold uppercase text-gray-500 tracking-widest">Core concepts</div>
-    <ul>
-      <li className="mt-4">
-        <Link href="/routing">
-          <a className="hover:underline font-medium text-gray-700">Routing</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/responses">
-          <a className="hover:underline font-medium text-gray-700">Responses</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/requests">
-          <a className="hover:underline font-medium text-gray-700">Requests</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/redirects">
-          <a className="hover:underline font-medium text-gray-700">Redirects</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/pages">
-          <a className="hover:underline font-medium text-gray-700">Pages</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/links">
-          <a className="hover:underline font-medium text-gray-700">Links</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/forms">
-          <a className="hover:underline font-medium text-gray-700">Forms</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/shared-data">
-          <a className="hover:underline font-medium text-gray-700">Shared data</a>
-        </Link>
-      </li>
-    </ul>
-    <div className="mt-12 text-xs font-bold uppercase text-gray-500 tracking-widest">Advanced</div>
-    <ul>
-      <li className="mt-4">
-        <Link href="/security">
-          <a className="hover:underline font-medium text-gray-700">Security</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/error-handling">
-          <a className="hover:underline font-medium text-gray-700">Error handling</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/asset-versioning">
-          <a className="hover:underline font-medium text-gray-700">Asset versioning</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/local-state-caching">
-          <a className="hover:underline font-medium text-gray-700">Local state caching</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/transforming-props">
-          <a className="hover:underline font-medium text-gray-700">Transforming props</a>
-        </Link>
-      </li>
-      <li className="mt-4">
-        <Link href="/server-side-rendering">
-          <a className="hover:underline font-medium text-gray-700">Server-side rendering</a>
-        </Link>
-      </li>
-    </ul>
-  </nav>
-)
+export default ({ className }) => {
+  const router = useRouter()
+
+  function linkClass(path) {
+    return router.pathname === path
+      ? `block px-2 py-2 rounded font-medium bg-purple-200 text-purple-800`
+      : `inline-block px-2 py-2 hover:text-purple-800 hover:underline font-medium text-gray-700`
+  }
+
+  return (
+    <nav className={className}>
+      <div className="mb-3 text-xs font-bold uppercase text-gray-500 tracking-widest">Getting started</div>
+      <ul>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/">
+            <a className={linkClass('/')}>Introduction</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/how-it-works">
+            <a className={linkClass('/how-it-works')}>How it works</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/installation">
+            <a className={linkClass('/installation')}>Installation</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/server-side-setup">
+            <a className={linkClass('/server-side-setup')}>Server-side setup</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/client-side-setup">
+            <a className={linkClass('/client-side-setup')}>Client-side setup</a>
+          </Link>
+        </li>
+      </ul>
+      <div className="mt-12 mb-3 text-xs font-bold uppercase text-gray-500 tracking-widest">Core concepts</div>
+      <ul>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/routing">
+            <a className={linkClass('/routing')}>Routing</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/responses">
+            <a className={linkClass('/responses')}>Responses</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/requests">
+            <a className={linkClass('/requests')}>Requests</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/redirects">
+            <a className={linkClass('/redirects')}>Redirects</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/pages">
+            <a className={linkClass('/pages')}>Pages</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/links">
+            <a className={linkClass('/links')}>Links</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/forms">
+            <a className={linkClass('/forms')}>Forms</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/shared-data">
+            <a className={linkClass('/shared-data')}>Shared data</a>
+          </Link>
+        </li>
+      </ul>
+      <div className="mt-12 mb-3 text-xs font-bold uppercase text-gray-500 tracking-widest">Advanced</div>
+      <ul>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/security">
+            <a className={linkClass('/security')}>Security</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/error-handling">
+            <a className={linkClass('/error-handling')}>Error handling</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/asset-versioning">
+            <a className={linkClass('/asset-versioning')}>Asset versioning</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/local-state-caching">
+            <a className={linkClass('/local-state-caching')}>Local state caching</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/transforming-props">
+            <a className={linkClass('/transforming-props')}>Transforming props</a>
+          </Link>
+        </li>
+        <li className="-ml-2 md:pr-4">
+          <Link href="/server-side-rendering">
+            <a className={linkClass('/server-side-rendering')}>Server-side rendering</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
