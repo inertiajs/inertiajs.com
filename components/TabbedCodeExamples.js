0dd6d18070c4e8b78fefc32f828d3362250e8094
Jonathan Reinink <jonathan@reinink.ca>
2019-09-30 20:11:47 -0400

    Improve style of tabbed code examples description

--- components/TabbedCodeExamples.js
+++ components/TabbedCodeExamples.js
@@ -23,11 +23,13 @@ export default ({ className, examples, height }) => {
         {examples[activeTab].code}
       </Code>
       {examples[activeTab].description && (
-        <div className="px-4 py-3 text-sm font-medium text-white flex items-center" css={{ background: '#303f6d' }}>
-          <svg className="w-4 h-4 fill-current mr-2" viewBox="0 0 20 20">
-            <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zM9 11v4h2V9H9v2zm0-6v2h2V5H9z" />
-          </svg>
-          <div>{examples[activeTab].description}</div>
+        <div className="p-4 text-sm font-medium text-white flex items-baseline" css={{ background: '#303f6d' }}>
+          <div className="flex-shrink-0 w-4 h-4 fill-current mr-2">
+            <svg className="mt-1 w-full h-full" viewBox="0 0 20 20">
+              <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zM9 11v4h2V9H9v2zm0-6v2h2V5H9z" />
+            </svg>
+          </div>
+          <div className="leading-snug">{examples[activeTab].description}</div>
         </div>
       )}
     </div>
