763db9f9595e8e4da65d681fb9877b1c8d3dae3b
Jonathan Reinink <jonathan@reinink.ca>
2019-11-27 11:29:12 -0500

    Tweak sponsorship wording

--- components/Layout.js
+++ components/Layout.js
@@ -88,7 +88,7 @@ export default function Layout({ meta, children }) {
           >
             <path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z" />
           </svg>
-          <div className="mx-2">Sponsor the ongoing development of Inertia.js.</div>
+          <div className="mx-2">Support the ongoing development of Inertia.js.</div>
           <Link href="/sponsors">
             <a className="px-3 py-1 bg-purple-500 hover:bg-purple-600 rounded-full text-xs text-white leading-tight whitespace-no-wrap">
               Learn more
