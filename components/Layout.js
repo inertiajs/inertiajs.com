d6111dd810cd899f15d99409b8ebcf5d27996cbb
Jonathan Reinink <jonathan@reinink.ca>
2019-11-28 11:14:33 -0500

    More layout fixes

--- components/Layout.js
+++ components/Layout.js
@@ -262,9 +262,12 @@ export default function Layout({ meta, children }) {
         </div>
       )}
 
-      <div className="max-w-6xl mx-auto px-6 md:px-12 xl:px-0 py-12 md:py-24 flex">
+      <div className="max-w-6xl mx-auto md:px-12 xl:px-0 py-12 md:py-24 flex">
         <Nav className="hidden md:block md:w-48 lg:w-56 flex-shrink-0 border-r" />
-        <div className="flex-1 md:overflow-hidden md:pl-12 lg:pl-16 xl:pl-16 xl:pr-20 leading-relaxed text-lg" id="top">
+        <div
+          className="flex-1 md:overflow-hidden px-6 md:pl-12 md:pr-0 lg:pl-16 xl:pl-16 xl:pr-20 leading-relaxed text-lg"
+          id="top"
+        >
           <MDXProvider components={MarkdownComponents} children={children} />
         </div>
         <div className="hidden xl:block w-44 flex-shrink-0 relative -mt-8">
