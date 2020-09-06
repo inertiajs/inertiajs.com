5ab3a30f4353dd2459e2d09bb49ae47cf626937b
Richard Keep <r.kipsang@gmail.com>
2020-09-06 19:39:46 +0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -190,7 +190,7 @@ export default function Layout({ meta, children }) {
                             {
                                 $users = User::active()
                                     ->orderByName()
-                                    ->get(['id', 'name', 'email']);
+                                    ->get('id', 'name', 'email');
 
                                 return Inertia::render('Users', [
                                     'users' => $users
