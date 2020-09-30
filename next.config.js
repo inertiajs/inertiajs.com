96d2fd95ac5c91a2f829129059f422a6d5b75001
Jonathan Reinink <jonathan@reinink.ca>
2020-09-30 16:21:17 -0400

    Update redirects config

--- next.config.js
+++ next.config.js
@@ -2,13 +2,4 @@ const withMDX = require('@next/mdx')()
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx'],
-  async redirects() {
-    return [
-      {
-        source: '/requests',
-        destination: '/manual-visits',
-        permanent: true,
-      },
-    ]
-  },
 })
