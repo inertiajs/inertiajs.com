b581fd857933248abc3df4bb6d424d94b948ad64
Jonathan Reinink <jonathan@reinink.ca>
2021-05-05 08:04:41 -0400

    Update Discord url

--- components/Layout.js
+++ components/Layout.js
@@ -176,7 +176,7 @@ export default function Layout({ meta, children }) {
                   <path d="M6.29 18.25c7.55 0 11.67-6.25 11.67-11.67v-.53c.8-.59 1.49-1.3 2.04-2.13-.75.33-1.54.55-2.36.65a4.12 4.12 0 0 0 1.8-2.27c-.8.48-1.68.81-2.6 1a4.1 4.1 0 0 0-7 3.74 11.65 11.65 0 0 1-8.45-4.3 4.1 4.1 0 0 0 1.27 5.49C2.01 8.2 1.37 8.03.8 7.7v.05a4.1 4.1 0 0 0 3.3 4.03 4.1 4.1 0 0 1-1.86.07 4.1 4.1 0 0 0 3.83 2.85A8.23 8.23 0 0 1 0 16.4a11.62 11.62 0 0 0 6.29 1.84" />
                 </svg>
               </a>
-              <a className="block flex items-center hover:text-purple-900" href="https://discord.gg/gwgxN8Y">
+              <a className="block flex items-center hover:text-purple-900" href="https://discord.gg/inertiajs">
                 <svg className="fill-current w-6 h-6" viewBox="0 0 146 146">
                   <title>Discord</title>
                   <path
