ed5efc91f0a1dc034c91a4bb9028244eb30c64ff
Jonathan Reinink <jonathan@reinink.ca>
2021-05-26 12:39:45 -0400

    Update link component to not make hash links Inertia links

--- resources/js/Components/A.js
+++ resources/js/Components/A.js
@@ -1,7 +1,7 @@
 import { InertiaLink } from '@inertiajs/inertia-react'
 
 export default function A(props) {
-  return props.href.startsWith('http') ? (
+  return props.href.startsWith('http') || props.href.startsWith('#') ? (
     <a {...props} className="text-blue-700 hover:text-orange-700 font-medium underline" />
   ) : (
     <InertiaLink {...props} className="text-blue-700 hover:text-orange-700 font-medium underline" />
