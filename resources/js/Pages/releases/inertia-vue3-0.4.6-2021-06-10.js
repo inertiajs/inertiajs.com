f4d2830f29061ab2ef82496ff94b788e09e207a1
Jonathan Reinink <jonathan@reinink.ca>
2021-06-10 15:43:17 -0400

    Tweak inertia-vue3@0.4.6 release

--- resources/js/Pages/releases/inertia-vue3-0.4.6-2021-06-10.js
+++ resources/js/Pages/releases/inertia-vue3-0.4.6-2021-06-10.js
@@ -5,8 +5,7 @@ import release from '@/Utils/release'
 export default release(
   <>
     <P>
-      Disable layout attribute inheritance in Vue 3 adapter (
-      <A href="https://github.com/inertiajs/inertia/pull/740">#740</A>).
+      Disable layout attribute inheritance (<A href="https://github.com/inertiajs/inertia/pull/740">#740</A>).
     </P>
   </>
 )
