2228bd49a15c991605b2f858197d48a609761a5b
Jonathan Reinink <jonathan@reinink.ca>
2021-07-14 11:26:04 -0400

    Add latest @inertiajs/progress releases

---
+++ resources/js/Pages/releases/progress-0.2.6-2020-07-14.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import release from '@/Utils/release'
+import { Code, P } from '@/Components'
+
+export default release(
+  <>
+    <P>
+      Add peer dependency support for <Code>@inertiajs/inertia</Code> version <Code>0.10.0</Code>.
+    </P>
+  </>
+)
