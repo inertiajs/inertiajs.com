8e8cb68a065d0fc5bcc9a196f6ab34a8f37c4fcf
Jonathan Reinink <jonathan@reinink.ca>
2021-08-02 20:59:17 -0400

    Add latest releases

---
+++ resources/js/Pages/releases/inertia-svelte-0.7.4-2021-08-02.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import release from '@/Utils/release'
+import { A, Code, Li, Ul } from '@/Components'
+
+export default release(
+  <>
+    <Ul>
+      <Li>
+        Fix links with <Code>null</Code> or <Code>undefined</Code> URLs (
+        <A href="https://github.com/inertiajs/inertia/pull/843">#843</A>).
+      </Li>
+    </Ul>
+  </>
+)
