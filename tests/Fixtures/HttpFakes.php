fa6ea572f533923ea5670a410788175fd0a7c317
Claudio Dekker <claudio@ubient.net>
2021-09-04 00:01:50 +0200

    Unit: The Github user/org is not being sponsored by the token owner

--- tests/Fixtures/HttpFakes.php
+++ tests/Fixtures/HttpFakes.php
@@ -33,6 +33,33 @@ class HttpFakes
         ]);
     }
 
+    public static function githubSponsorsViewerIsNotSponsoringUser(): void
+    {
+        Http::fake([
+            'https://api.github.com/graphql' => Http::response([
+                'data' => [
+                    'user' => [
+                        'viewerIsSponsoring' => false,
+                    ],
+                    'organization' => null,
+                ],
+                'errors' => [
+                    [
+                        'type' => 'NOT_FOUND',
+                        'path' => [
+                            'organization',
+                        ],
+                        'locations' => [
+                            'line' => 5,
+                            'column' => 3,
+                        ],
+                        'message' => "Could not resolve to an Organization with the login of 'reinink'.",
+                    ],
+                ],
+            ]),
+        ]);
+    }
+
     public static function githubSponsorsViewerIsSponsoringOrganization(): void
     {
         Http::fake([
@@ -41,6 +68,33 @@ class HttpFakes
                     'user' => null,
                     'organization' => [
                         'viewerIsSponsoring' => true,
+                    ],
+                ],
+                'errors' => [
+                    [
+                        'type' => 'NOT_FOUND',
+                        'path' => [
+                            'user',
+                        ],
+                        'locations' => [
+                            'line' => 2,
+                            'column' => 7,
+                        ],
+                        'message' => "Could not resolve to a User with the login of 'laravel'.",
+                    ],
+                ],
+            ]),
+        ]);
+    }
+
+    public static function githubSponsorsViewerIsNotSponsoringOrganization(): void
+    {
+        Http::fake([
+            'https://api.github.com/graphql' => Http::response([
+                'data' => [
+                    'user' => null,
+                    'organization' => [
+                        'viewerIsSponsoring' => false,
                     ],
                 ],
                 'errors' => [
--- tests/Unit/Services/Github/ApiTest.php
+++ tests/Unit/Services/Github/ApiTest.php
@@ -28,6 +28,23 @@ class ApiTest extends TestCase
     }
 
     /** @test */
+    public function the_github_user_is_not_being_sponsored_by_the_token_owner(): void
+    {
+        HttpFakes::githubSponsorsViewerIsNotSponsoringUser();
+
+        $response = app(GithubApi::class)->isSponsoring('bar', 'foo');
+
+        $this->assertFalse($response);
+        Http::assertSentCount(1);
+        Http::assertSent(function (Request $request) {
+            $this->assertSame('Bearer foo', $request->header('Authorization.0'));
+            $this->assertSame('bar', $request->data()['variables']['account']);
+
+            return true;
+        });
+    }
+
+    /** @test */
     public function the_github_organization_is_being_sponsored_by_the_token_owner(): void
     {
         HttpFakes::githubSponsorsViewerIsSponsoringOrganization();
@@ -35,6 +52,23 @@ class ApiTest extends TestCase
         $response = app(GithubApi::class)->isSponsoring('bar', 'foo');
 
         $this->assertTrue($response);
+        Http::assertSentCount(1);
+        Http::assertSent(function (Request $request) {
+            $this->assertSame('Bearer foo', $request->header('Authorization.0'));
+            $this->assertSame('bar', $request->data()['variables']['account']);
+
+            return true;
+        });
+    }
+
+    /** @test */
+    public function the_github_organization_is_not_being_sponsored_by_the_token_owner(): void
+    {
+        HttpFakes::githubSponsorsViewerIsNotSponsoringOrganization();
+
+        $response = app(GithubApi::class)->isSponsoring('bar', 'foo');
+
+        $this->assertFalse($response);
         Http::assertSentCount(1);
         Http::assertSent(function (Request $request) {
             $this->assertSame('Bearer foo', $request->header('Authorization.0'));
