c5e37ca224fc1a4eaa26306dccd8997b871fda0b
Claudio Dekker <claudio@ubient.net>
2022-01-08 13:24:23 +0100

    WIP

--- resources/js/Pages/releases/inertia-vue-0.8.0-2022-01-07.js
+++ resources/js/Pages/releases/inertia-vue-0.8.0-2022-01-07.js
@@ -6,23 +6,6 @@ import { CodeBlock, H2, P } from '@/Components'
 
 export default release(
   <Fragment>
-    <Markdown content={dedent`
-      ### Added
-
-      - Visits and \`Link\` components now accept a 'queryStringArrayFormat' option ([#994](https://github.com/inertiajs/inertia/pull/994))
-      - Add \`setError\` method to Form helper to manually set errors ([#999](https://github.com/inertiajs/inertia/pull/999))
-      - Add \`defaults\` method to Form helper to 'redefine' the defaults ([#1019](https://github.com/inertiajs/inertia/pull/1019))
-
-      ### Changed
-
-      - We now keep a changelog here on GitHub :tada: For earlier releases, please see [the releases page of inertiajs.com](https://inertiajs.com/releases?all=true#inertia-vue).
-      - Types: Use \`@inertiajs/inertia\`'s new 'Progress' type ([#877](https://github.com/inertiajs/inertia/pull/877))
-
-      ### Fixed
-
-      - Console warnings still referenced \`<inertia-link>\` instead of \`<Link>\` ([#916](https://github.com/inertiajs/inertia/pull/916))
-      - Push Inertia "plugin" to userland instead of auto-registering, similar to Vue 3 ([\`425663\`](https://github.com/inertiajs/inertia/commit/4256638b215e5a67d951baeab89aa6043c27e85d))
-    `} />
     <H2>Removed Inertia "plugin" auto-registration - BREAKING CHANGE ⚠️</H2>
     <P>
       This release removes the auto-registration of the Inertia "plugin",
@@ -44,5 +27,22 @@ export default release(
         })
       `}
     />
+    <Markdown content={dedent`
+      ### Added
+
+      - Visits and \`Link\` components now accept a 'queryStringArrayFormat' option ([#994](https://github.com/inertiajs/inertia/pull/994))
+      - Add \`setError\` method to Form helper to manually set errors ([#999](https://github.com/inertiajs/inertia/pull/999))
+      - Add \`defaults\` method to Form helper to 'redefine' the defaults ([#1019](https://github.com/inertiajs/inertia/pull/1019))
+
+      ### Changed
+
+      - We now keep a changelog here on GitHub :tada: For earlier releases, please see [the releases page of inertiajs.com](https://inertiajs.com/releases?all=true#inertia-vue).
+      - Types: Use \`@inertiajs/inertia\`'s new 'Progress' type ([#877](https://github.com/inertiajs/inertia/pull/877))
+      - Push Inertia "plugin" to userland instead of auto-registering, similar to Vue 3 ([\`425663\`](https://github.com/inertiajs/inertia/commit/4256638b215e5a67d951baeab89aa6043c27e85d))
+
+      ### Fixed
+
+      - Console warnings still referenced \`<inertia-link>\` instead of \`<Link>\` ([#916](https://github.com/inertiajs/inertia/pull/916))
+    `} />
   </Fragment>
 )
