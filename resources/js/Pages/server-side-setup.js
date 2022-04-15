0544d959a2ef991a580273acd991f3d9527d42c2
Taylor Otwell <taylor@laravel.com>
2022-04-15 16:27:18 -0500

    working on server side docs

--- resources/js/Pages/server-side-setup.js
+++ resources/js/Pages/server-side-setup.js
@@ -6,6 +6,7 @@ const meta = {
   title: 'Server-side setup',
   links: [
     { url: '#install-dependencies', name: 'Install dependencies' },
+    { url: '#laravel-starter-kits', name: 'Laravel starter kits' },
     { url: '#root-template', name: 'Root template' },
     { url: '#middleware', name: 'Middleware' },
     { url: '#creating-responses', name: 'Creating responses' },
@@ -17,14 +18,25 @@ const Page = () => {
     <>
       <H1>Server-side setup</H1>
       <P>
-        The first step when installing Inertia is to configure your server-side framework. Inertia ships with official
+        The first step when installing Inertia is to configure your server-side framework. Inertia maintains official
         server-side adapters for <A href="https://laravel.com/">Laravel</A> and{' '}
         <A href="https://rubyonrails.org/">Rails</A>. For other frameworks, please see the{' '}
         <A href="/community-adapters">community adapters</A>.
       </P>
+      <P>
+        Inertia is fine-tuned for Laravel, so the documentation examples on this website utilize Laravel. For examples
+        of using Inertia with other server-side frameworks, please refer to the framework specific documentation maintained
+        by that adapter.
+      </P>
+      <H2>Laravel starter kits</H2>
+      <P>
+        Laravel's <A href="https://laravel.com/docs/starter-kits">starter kits</A>, Breeze and Jetstream, provide out-of-the-box scaffolding
+        for new Inertia applications. These starter kits are the absolute fastest way to start building a new Inertia project using Laravel and Vue or React.
+        However, if you would like to manually install Inertia into your application, please consult the documentation below.
+      </P>
       <H2>Install dependencies</H2>
       <P>
-        Install the Inertia server-side adapters using the preferred package manager for that language or framework.
+        First, install the Inertia server-side adapter using the Composer package manager.
       </P>
       <TabbedCode
         examples={[
@@ -34,23 +46,14 @@ const Page = () => {
             code: dedent`
               composer require inertiajs/inertia-laravel
             `,
-            description: 'Install via Composer',
-          },
-          {
-            name: 'Rails',
-            language: 'ruby',
-            description: 'Add this to your Gemfile',
-            code: dedent`
-              gem 'inertia_rails'
-            `,
           },
         ]}
       />
       <H2>Root template</H2>
       <P>
-        Next, setup the root template that will be loaded on the first page visit. This will be used to load your site
-        assets (CSS and JavaScript), and will also contain a root <Code>{'<div>'}</Code> to boot your JavaScript
-        application in.
+        Next, setup the root template that will be loaded on the first page visit to your application. This will be used to
+        load your site assets (CSS and JavaScript), and will also contain a root <Code>{'<div>'}</Code> in which to boot your JavaScript
+        application.
       </P>
       <TabbedCode
         examples={[
@@ -72,34 +75,18 @@ const Page = () => {
                 </body>
               </html>
             `,
-            description: `By default the Laravel adapter will use the app.blade.php view. This template should include your assets, as well as the @inertia directive. If you'd like to use a different root view, you can change it using Inertia::setRootView().`,
-          },
-          {
-            name: 'Rails',
-            language: 'erb',
-            code: dedent`
-              <!DOCTYPE html>
-              <html>
-                <head>
-                  <meta charset="utf-8" />
-                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                  <%= stylesheet_pack_tag 'application' %>
-                  <%= javascript_pack_tag 'application', defer: true %>
-                </head>
-                <body>
-                  <%= yield %>
-                </body>
-              </html>
-            `,
-            description: `By default the Rails adapter will use the application.html.erb view. This template should include your assets, as well as the yield command. If you'd like to use a different root view, you can change it using InertiaRails.configure.`,
           },
         ]}
       />
+      <P>
+        By default, Inertia's Laravel adapter will assume your root template is named <Code>app.blade.php</Code>. This template
+        should include your assets, as well as the <Code>@inertia</Code> directive. If you would like to use a different root
+        view, you can change it using the <Code>Inertia::setRootView()</Code> method.
+      </P>
       <H2>Middleware</H2>
       <P>
-        Next, setup the Inertia middleware. In the Rails adapter, this is configured automatically for you. However, in
-        Laravel you need to publish the <Code>HandleInertiaRequests</Code> middleware to your application, which can be
-        done using this artisan command:
+        Next we need to setup the Inertia middleware. You can accomplish this by publishing the <Code>HandleInertiaRequests</Code> middleware to your application, which can be
+        done using the following Artisan command.
       </P>
       <CodeBlock
         language="sh"
@@ -108,8 +95,8 @@ const Page = () => {
         `}
       />
       <P>
-        Once generated, register the <Code>HandleInertiaRequests</Code> middleware in your <Code>App\Http\Kernel</Code>,
-        as the <u>last item</u> in your <Code>web</Code> middleware group.
+        Once the middleware has been published, register the <Code>HandleInertiaRequests</Code> middleware in your <Code>App\Http\Kernel</Code> as
+        the <u>last item</u> in your <Code>web</Code> middleware group.
       </P>
       <CodeBlock
         language="php"
@@ -122,8 +109,8 @@ const Page = () => {
       />
       <P>
         This middleware provides a <Code>version()</Code> method for setting your{' '}
-        <A href="/asset-versioning">asset version</A>, and a <Code>share()</Code> method for setting{' '}
-        <A href="/shared-data">shared data</A>. Please see those pages for more information.
+        <A href="/asset-versioning">asset version</A>, as well as a <Code>share()</Code> method for defining{' '}
+        <A href="/shared-data">shared data</A>.
       </P>
       <H2>Creating responses</H2>
       <P>
