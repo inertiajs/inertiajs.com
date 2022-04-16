f99f77cd58c6ba3431b2e9c134ab314a4876ba7f
Taylor Otwell <taylor@laravel.com>
2022-04-16 13:00:21 -0500

    work on links page

--- resources/js/Pages/links.js
+++ resources/js/Pages/links.js
@@ -23,14 +23,14 @@ const Page = () => {
     <>
       <H1>Links</H1>
       <P>
-        To create links within an Inertia app you'll need to use the Inertia link component. This is a light wrapper
+        To create links to other pages within an Inertia app, you will typically use the Inertia <Code>{'<Link>'}</Code> component. This component is a light wrapper
         around a standard anchor <Code>{'<a>'}</Code> link that intercepts click events and prevents full page reloads
-        from occurring. This is how Inertia provides a single-page app experience.
+        from occurring. This is <A href="/how-it-works">how Inertia provides a single-page app experience</A> once your application has been loaded.
       </P>
       <H2>Creating links</H2>
       <P>
-        To create an Inertia link, use the Inertia <Code>{'<Link>'}</Code> component. Note, any attributes you provide
-        will be proxied to the underlying tag.
+        To create an Inertia link, use the Inertia <Code>{'<Link>'}</Code> component. Any attributes you provide to this component
+        will be proxied to the underlying HTML tag.
       </P>
       <TabbedCode
         examples={[
@@ -71,7 +71,7 @@ const Page = () => {
         ]}
       />
       <P>
-        By default Inertia renders links as anchor <Code>{'<a>'}</Code> elements. However, you can change the tag using
+        By default, Inertia renders links as anchor <Code>{'<a>'}</Code> elements. However, you can change the tag using
         the <Code>as</Code> attribute.
       </P>
       <TabbedCode
@@ -82,7 +82,7 @@ const Page = () => {
             code: dedent`
               import { Link } from '@inertiajs/inertia-vue'\n
               <Link href="/logout" method="post" as="button" type="button">Logout</Link>\n
-              // Renders as:
+              // Renders as...
               <button type="button">Logout</button>
             `,
           },
@@ -92,7 +92,7 @@ const Page = () => {
             code: dedent`
               import { Link } from '@inertiajs/inertia-vue3'\n
               <Link href="/logout" method="post" as="button" type="button">Logout</Link>\n
-              // Renders as:
+              // Renders as...
               <button type="button">Logout</button>
             `,
           },
@@ -102,7 +102,7 @@ const Page = () => {
             code: dedent`
               import { Link } from '@inertiajs/inertia-react'\n
               <Link href="/logout" method="post" as="button" type="button">Logout</Link>\n
-              // Renders as:
+              // Renders as...
               <button type="button">Logout</button>
             `,
           },
@@ -112,24 +112,24 @@ const Page = () => {
             code: dedent`
               import { inertia } from '@inertiajs/inertia-svelte'\n
               <button use:inertia="{{ href: '/logout', method: 'post' }}" type="button">Logout</button>\n
-              // Renders as:
+              // Renders as...
               <button type="button">Logout</button>
             `,
             description:
-              'Svelte does not support dynamic elements yet, but you can use the inertia directive to achieve the same results.',
+              'Svelte does not support dynamic elements yet, but you can use the "inertia" directive to achieve the same results.',
           },
         ]}
       />
       <Notice>
         Creating <Code color="orange">POST</Code>/<Code color="orange">PUT</Code>/<Code color="orange">PATCH</Code>/
         <Code color="orange">DELETE</Code> anchor <Code color="orange">{'<a>'}</Code> links is discouraged as it causes
-        "Open Link in New Tab/Window" accessibility issues. Instead, consider using a more appropriate element, such as
+        "Open Link in New Tab / Window" accessibility issues. Instead, consider using a more appropriate element, such as
         a <Code color="orange">{'<button>'}</Code>.
       </Notice>
       <H2>Method</H2>
       <P>
-        You can specify the method for an Inertia link request. The default is <Code>GET</Code>, but you can also use{' '}
-        <Code>POST</Code>, <Code>PUT</Code>, <Code>PATCH</Code>, and <Code>DELETE</Code>.
+        You can specify the HTTP request method for an Inertia link request using the <Code>method</Code> attribute. The default method used by links is <Code>GET</Code>, but you
+        can use the <Code>method</Code> attribute to make <Code>POST</Code>, <Code>PUT</Code>, <Code>PATCH</Code>, and <Code>DELETE</Code> requests via links.
       </P>
       <TabbedCode
         examples={[
@@ -138,7 +138,7 @@ const Page = () => {
             language: 'jsx',
             code: dedent`
               import { Link } from '@inertiajs/inertia-vue'\n
-              <Link href="/logout" method="post">Logout</Link>
+              <Link href="/logout" method="post" as="button">Logout</Link>
             `,
           },
           {
@@ -146,7 +146,7 @@ const Page = () => {
             language: 'jsx',
             code: dedent`
               import { Link } from '@inertiajs/inertia-vue3'\n
-              <Link href="/logout" method="post">Logout</Link>
+              <Link href="/logout" method="post" as="button">Logout</Link>
             `,
           },
           {
@@ -154,7 +154,7 @@ const Page = () => {
             language: 'jsx',
             code: dedent`
               import { Link } from '@inertiajs/inertia-react'\n
-              <Link href="/logout" method="post">Logout</Link>
+              <Link href="/logout" method="post" as="button">Logout</Link>
             `,
           },
           {
@@ -162,16 +162,15 @@ const Page = () => {
             language: 'jsx',
             code: dedent`
               import { inertia, Link } from '@inertiajs/inertia-svelte'\n
-              <a href="/logout" use:inertia="{{ method: 'post' }}">Logout</a>\n
-              <Link href="/logout" method="post">Logout</Link>
+              <button use:inertia="{{ href: '/logout', method: 'post' }}" type="button">Logout</button>\n
             `,
           },
         ]}
       />
       <H2>Data</H2>
       <P>
-        You can add data using the <Code>data</Code> attribute. This can be an <Code>object</Code>, or a{' '}
-        <Code>FormData</Code> instance.
+        When making <Code>POST</Code> or <Code>PUT</Code> requests, you may wish to add additional data to the request. You can accomplish this using the <Code>data</Code> attribute.
+        The provided data can be an <Code>object</Code> or <Code>FormData</Code> instance.
       </P>
       <TabbedCode
         examples={[
@@ -211,8 +210,8 @@ const Page = () => {
       />
       <H2>Headers</H2>
       <P>
-        The <Code>headers</Code> option allows you to add custom headers to an Inertia link. Do note that Inertia's
-        headers take priority and therefore cannot be overwritten.
+        The <Code>headers</Code> attribute allows you to add custom headers to an Inertia link. However, you should note that the headers Inertia
+        uses internally to communicate its state to the server take priority and therefore cannot be overwritten.
       </P>
       <TabbedCode
         examples={[
@@ -253,10 +252,10 @@ const Page = () => {
       />
       <H2>Replace</H2>
       <P>
-        You can specify the browser history behaviour. By default page visits push (new) state (
-        <Code>window.history.pushState</Code>) into the history, however it's also possible to replace state (
+        The <Code>replace</Code> attribute allows you to specify the browser's history behaviour. By default, page visits push (new) state (
+        <Code>window.history.pushState</Code>) into the history; however, it's also possible to replace state (
         <Code>window.history.replaceState</Code>) by setting the <Code>replace</Code> attribute to true. This will cause
-        the visit to replace the current history state, instead of adding a new history state to the stack.
+        the visit to replace the current history state instead of adding a new history state to the stack.
       </P>
       <TabbedCode
         examples={[
@@ -298,8 +297,8 @@ const Page = () => {
       <H2>Preserve state</H2>
       <P>
         You can preserve a page component's local state using the <Code>preserve-state</Code> attribute. This will
-        prevent a page component from fully re-rendering. This is especially helpful with forms, since you can avoid
-        manually repopulating input fields, and can also maintain a focused input.
+        prevent a page component from fully re-rendering. The <Code>preserve-state</Code> attribute is especially helpful with forms, since you can avoid
+        manually repopulating input fields and can also maintain a focused input.
       </P>
       <TabbedCode
         examples={[
@@ -345,7 +344,7 @@ const Page = () => {
       <H2>Preserve scroll</H2>
       <P>
         You can use the <Code>preserve-scroll</Code> attribute to prevent Inertia from automatically resetting the
-        scroll position when making the visit.
+        scroll position when making a page visit.
       </P>
       <TabbedCode
         examples={[
@@ -385,12 +384,12 @@ const Page = () => {
         ]}
       />
       <P>
-        For more information, see the <A href="/scroll-management">scroll management</A> page.
+        For more information on managing scroll position, please consult the documentation on <A href="/scroll-management">scroll management</A>.
       </P>
       <H2>Partial reloads</H2>
       <P>
-        The <Code>only</Code> option allows you to request a subset of the props (data) from the server on subsequent
-        visits to the same page.
+        The <Code>only</Code> attribute allows you to specify that only a subset of a page's props (data) should be retrieved from the server on subsequent
+        visits to that page.
       </P>
       <TabbedCode
         examples={[
@@ -430,12 +429,12 @@ const Page = () => {
         ]}
       />
       <P>
-        For more information, see the <A href="/partial-reloads">partial reloads</A> page.
+        For more information on this topic, please consult the complete documentation on <A href="/partial-reloads">partial reloads</A>.
       </P>
       <H2>Active states</H2>
       <P>
-        It's often desireable to set an active state for navigation links based on the current page. This can be done in
-        Inertia using the <Code>page</Code> object by doing string comparisons against the <Code>page.url</Code> and{' '}
+        It's often desirable to set an active state for navigation links based on the current page. This can be accomplished when using
+        Inertia by inspecting the <Code>page</Code> object and doing string comparisons against the <Code>page.url</Code> and{' '}
         <Code>page.component</Code> properties.
       </P>
       <TabbedCode
@@ -445,13 +444,13 @@ const Page = () => {
             language: 'jsx',
             code: dedent`
               import { Link } from '@inertiajs/inertia-vue'\n
-              // URL exact match
+              // URL exact match...
               <Link href="/users" :class="{ 'active': $page.url === '/users' }">Users</Link>\n
-              // Component exact match
+              // Component exact match...
               <Link href="/users" :class="{ 'active': $page.component === 'Users/Index' }">Users</Link>\n
-              // URL starts with (/users, /users/create, /users/1, etc.)
+              // URL starts with (/users, /users/create, /users/1, etc.)...
               <Link href="/users" :class="{ 'active': $page.url.startsWith('/users') }">Users</Link>\n
-              // Component starts with (Users/Index, Users/Create, Users/Show, etc.)
+              // Component starts with (Users/Index, Users/Create, Users/Show, etc.)...
               <Link href="/users" :class="{ 'active': $page.component.startsWith('Users') }">Users</Link>
             `,
           },
@@ -460,13 +459,13 @@ const Page = () => {
             language: 'jsx',
             code: dedent`
               import { Link } from '@inertiajs/inertia-vue'\n
-              // URL exact match
+              // URL exact match...
               <Link href="/users" :class="{ 'active': $page.url === '/users' }">Users</Link>\n
-              // Component exact match
+              // Component exact match...
               <Link href="/users" :class="{ 'active': $page.component === 'Users/Index' }">Users</Link>\n
-              // URL starts with (/users, /users/create, /users/1, etc.)
+              // URL starts with (/users, /users/create, /users/1, etc.)...
               <Link href="/users" :class="{ 'active': $page.url.startsWith('/users') }">Users</Link>\n
-              // Component starts with (Users/Index, Users/Create, Users/Show, etc.)
+              // Component starts with (Users/Index, Users/Create, Users/Show, etc.)...
               <Link href="/users" :class="{ 'active': $page.component.startsWith('Users') }">Users</Link>
             `,
           },
@@ -476,13 +475,13 @@ const Page = () => {
             code: dedent`
               import { usePage } from '@inertiajs/inertia-react'\n
               const { url, component } = usePage()\n
-              // URL exact match
+              // URL exact match...
               <Link href="/users" className={url === '/users' ? 'active' : ''}>Users</Link>\n
-              // Component exact match
+              // Component exact match...
               <Link href="/users" className={component === 'Users/Index' ? 'active' : ''}>Users</Link>\n
-              // URL starts with (/users, /users/create, /users/1, etc.)
+              // URL starts with (/users, /users/create, /users/1, etc.)...
               <Link href="/users" className={url.startsWith('/users') === '/users' ? 'active' : ''}>Users</Link>\n
-              // Component starts with (Users/Index, Users/Create, Users/Show, etc.)
+              // Component starts with (Users/Index, Users/Create, Users/Show, etc.)...
               <Link href="/users" className={component.startsWith('Users') === 'Users/Index' ? 'active' : ''}>Users</Link>
             `,
           },
@@ -491,25 +490,25 @@ const Page = () => {
             language: 'jsx',
             code: dedent`
               import { page } from '@inertiajs/inertia-svelte'\n
-              // URL exact match
+              // URL exact match...
               <Link href="/users" class={$page.url === '/users' ? 'active' : ''}>Users</Link>\n
-              // Component exact match
+              // Component exact match...
               <Link href="/users" class={$page.component === 'Users/Index' ? 'active' : ''}>Users</Link>\n
-              // URL starts with (/users, /users/create, /users/1, etc.)
+              // URL starts with (/users, /users/create, /users/1, etc.)...
               <Link href="/users" class={$page.url.startsWith('/users') === '/users' ? 'active' : ''}>Users</Link>\n
-              // Component starts with (Users/Index, Users/Create, Users/Show, etc.)
+              // Component starts with (Users/Index, Users/Create, Users/Show, etc.)...
               <Link href="/users" class={$page.component.startsWith('Users') === 'Users/Index' ? 'active' : ''}>Users</Link>
             `,
           },
         ]}
       />
       <P>
-        You can do exact matches (<Code>===</Code>), or <Code>startsWith()</Code> checks (useful for matching a subset
+        You can perform exact match comparisons (<Code>===</Code>), <Code>startsWith()</Code> comparisons (useful for matching a subset
         of pages), or even more complex comparisons using regular expressions.
       </P>
       <P>
-        What's nice about this approach is that you're not limited to just setting class names. You can use this
-        technique to conditionally render any markup on active state, such as different link text, or even an SVG icon.
+        Using this approach, you're not limited to just setting class names. You can use this
+        technique to conditionally render any markup on active state, such as different link text or even an SVG icon that represents the link is active.
       </P>
     </>
   )
