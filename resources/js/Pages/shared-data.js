635fd51fafd3ee9deab5d5eb55313c0a1fc8a8e4
Taylor Otwell <taylor@laravel.com>
2022-04-22 16:03:13 -0500

    shared data refactor

--- resources/js/Pages/shared-data.js
+++ resources/js/Pages/shared-data.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import dedent from 'dedent-js'
-import { H1, H2, Layout, Notice, P, TabbedCode } from '@/Components'
+import { A, Code, H1, H2, Layout, Notice, P, TabbedCode } from '@/Components'
 
 const meta = {
   title: 'Shared data',
@@ -17,14 +17,16 @@ const Page = () => {
     <>
       <H1>Shared data</H1>
       <P>
-        Sometimes you need to access certain data on numerous pages within your application. For example, a common
-        use-case for this is showing the current user in the site header. Passing this data manually in each response
-        isn't practical. In these situations shared data can be useful.
+        Sometimes you need to access specific pieces of data on numerous pages within your application. For example, you may need to display the current user in the site header. Passing this data manually in each response
+        across your entire application isn't practical. Thankfully, there is a better option: shared data.
       </P>
       <H2>Sharing data</H2>
       <P>
-        The server-side adapters provide a way to preassign shared data for each request. This is typically done outside
+        Inertia's server-side adapters provide a method of preassigning shared data for each request. This is typically done outside
         of your controllers. Shared data will be automatically merged with the page props provided in your controller.
+      </P>
+      <P>
+        In Laravel applications, this is typically handled by the <Code>HandleInertiaRequests</Code> middleware that is automatically installed when installing the <A href="/server-side-setup#middleware">server-side adapter</A>.
       </P>
       <TabbedCode
         examples={[
@@ -32,71 +34,55 @@ const Page = () => {
             name: 'Laravel',
             language: 'php',
             code: dedent`
-              /*
-              |----------------------------------------------------------------
-              | Via the HandleInertiaRequests middleware (recommended)
-              |----------------------------------------------------------------
-              */\n
               class HandleInertiaRequests extends Middleware
               {
                   public function share(Request $request)
                   {
-                      return array_merge(parent::share($request), [\n
-                          // Synchronously
+                      return array_merge(parent::share($request), [
+                          // Synchronously...
                           'appName' => config('app.name'),\n
-                          // Lazily
+                          // Lazily...
                           'auth.user' => fn () => $request->user()
                               ? $request->user()->only('id', 'name', 'email')
                               : null,
                       ]);
                   }
-              }\n
-              /*
-              |----------------------------------------------------------------
-              | Manually
-              |----------------------------------------------------------------
-              */\n
+              }
+            `,
+            description:
+              'The HandleInertiaRequests middleware provides a "share" method where you can define the data that is automatically shared with each Inertia response.',
+          }
+        ]}
+      />
+      <P>
+        Alternatively, you can manually share data using the <Code>Inertia::share</Code> method.
+      </P>
+      <TabbedCode
+        examples={[
+          {
+            name: 'Laravel',
+            language: 'php',
+            code: dedent`
               use Inertia\\Inertia;\n
-              // Synchronously
+              // Synchronously...
               Inertia::share('appName', config('app.name'));\n
-              // Lazily
+              // Lazily...
               Inertia::share('user', fn (Request $request) => $request->user()
                   ? $request->user()->only('id', 'name', 'email')
                   : null
               );
-            `,
-            description:
-              'The HandleInertiaRequests middleware provides a "share" method where you can define the data that is automatically shared with each Inertia response.',
-          },
-          {
-            name: 'Rails',
-            language: 'ruby',
-            code: dedent`
-              # Synchronously
-              inertia_share app_name: env['app.name']\n
-              # Lazily
-              inertia_share do
-                if logged_in?
-                  {
-                    'auth.user' => {id: logged_in_user.id}
-                  }
-                end
-              end\n
-              # Lazily
-              inertia_share user_count: lambda { User.count }
-            `,
-          },
+            `
+          }
         ]}
       />
-      <Notice>Use this feature sparingly as shared data is included with every response.</Notice>
+      <Notice>Shared data should be used sparingly as all shared data is included with every response.</Notice>
       <Notice>
-        Page props and shared data are merged together, so be sure to namespace your shared data appropriately.
+        Page props and shared data are merged together, so be sure to namespace your shared data appropriately to avoid collisions.
       </Notice>
       <H2>Accessing shared data</H2>
       <P>
-        Once you've shared the data server-side, you'll then be able to access it within your page components as props.
-        Shared data can even be accessed in non-page components, although not as props in those cases. Here's an example
-        of how to do this in a layout component.
+        Once you have shared the data server-side, you will then be able to access it within any of your pages or components.
+        Here's an example of how to access shared data in a layout component.
       </P>
       <TabbedCode
         examples={[
@@ -123,8 +109,7 @@ const Page = () => {
                 }
               }
               </script>
-            `,
-            description: 'Access shared data using the $page property.',
+            `
           },
           {
             name: 'Vue 3',
@@ -150,8 +135,7 @@ const Page = () => {
                 },
               }
               </script>
-            `,
-            description: 'Access shared data using the $page property or the usePage() hook.',
+            `
           },
           {
             name: 'React',
@@ -171,8 +155,7 @@ const Page = () => {
                   </main>
                 )
               }
-            `,
-            description: 'Access shared data using the usePage() hook.',
+            `
           },
           {
             name: 'Svelte',
@@ -189,15 +172,14 @@ const Page = () => {
                   <slot />
                 </content>
               </main>
-            `,
-            description: 'Access shared data using the $page store.',
+            `
           },
         ]}
       />
       <H2>Flash messages</H2>
       <P>
         Another great use-case for shared data is flash messages. These are messages stored in the session only for the
-        next request. You'll often set a flash message after completing a task and before redirecting to a different
+        next request. For example, it's common to set a flash message after completing a task and before redirecting to a different
         page.
       </P>
       <P>
@@ -222,20 +204,7 @@ const Page = () => {
                   }
               }
             `,
-          },
-          {
-            name: 'Rails',
-            language: 'ruby',
-            code: dedent`
-              class ApplicationController < ActionController::Base
-                inertia_share flash: -> {
-                  {
-                    message: flash.notice
-                  }
-                }
-              end
-            `,
-          },
+          }
         ]}
       />
       <P>Next, display the flash message in a front-end component, such as the site layout.</P>
