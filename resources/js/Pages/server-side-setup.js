21b275c597ec99139519c05482b2024c54734c06
robindrost <robindrost@gmail.com>
2022-08-25 05:13:21 +0200

    Update Mix to Vite
    Use Vite to load the js and css assets since its default now in Laravel.

--- resources/js/Pages/server-side-setup.js
+++ resources/js/Pages/server-side-setup.js
@@ -63,8 +63,7 @@ const Page = () => {
                 <head>
                   <meta charset="utf-8" />
                   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-                  <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-                  <script src="{{ mix('/js/app.js') }}" defer></script>
+                  @vite(['resources/css/app.css', 'resources/js/app.js'])
                   @inertiaHead
                 </head>
                 <body>
