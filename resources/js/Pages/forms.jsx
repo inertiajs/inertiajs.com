58cee276ef2b6d4c0bd5ccf5cb02bcd82b9907bd
Jonathan Reinink <jonathan@reinink.ca>
2023-01-12 21:41:27 -0500

    Fix Svelte form helper example

--- resources/js/Pages/forms.jsx
+++ resources/js/Pages/forms.jsx
@@ -350,9 +350,9 @@ export default function () {
                 {#if $form.errors.email}
                   <div class="form-error">{$form.errors.email}</div>
                 {/if}
-                <input type="text" bind:value={$form.email} />
-                {#if $form.errors.email}
-                  <div class="form-error">{$form.errors.email}</div>
+                <input type="password" bind:value={$form.password} />
+                {#if $form.errors.password}
+                  <div class="form-error">{$form.errors.password}</div>
                 {/if}
                 <input type="checkbox" bind:checked={$form.remember} /> Remember Me
                 <button type="submit" disabled={$form.processing}>Submit</button>
