ccf16ebc303ee7fc36f585c4ce3c54daddd74552
Taylor Otwell <taylor@laravel.com>
2023-04-13 14:34:25 -0500

    authorization fixes

--- resources/js/Pages/authorization.jsx
+++ resources/js/Pages/authorization.jsx
@@ -11,8 +11,8 @@ export default function () {
       <H1>Authorization</H1>
       <P>
         When using Inertia, authorization is best handled server-side in your application's authorization policies.
-        However, you may be wondering how to perform checks against your policies from within your JavaScript page
-        components since you won't have access to your server-side helpers.
+        However, you may be wondering how to perform checks against your authorization policies from within your Inertia page
+        components since you won't have access to your framework's server-side helpers.
       </P>
       <P>
         The simplest approach to solving this problem is to pass the results of your authorization checks as props to
