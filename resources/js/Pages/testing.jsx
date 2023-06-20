1c5b886c65007c1dcbdb6319fca052aeff2a8c45
TheBlckbird <66938161+TheBlckbird@users.noreply.github.com>
2023-06-20 21:33:25 +0200

    Update link to Laravel Dusk

--- resources/js/Pages/testing.jsx
+++ resources/js/Pages/testing.jsx
@@ -21,7 +21,7 @@ export default function () {
       <H2>End-to-end tests</H2>
       <P>
         One popular approach to testing your JavaScript page components is to use an end-to-end testing tool like{' '}
-        <A href="https://www.cypress.io/">Cypress</A> or <A href="https://laravel.com/docs/8.x/dusk">Laravel Dusk</A>.
+        <A href="https://www.cypress.io/">Cypress</A> or <A href="https://laravel.com/docs/dusk">Laravel Dusk</A>.
         These are browser automation tools that allow you to run real simulations of your app in the browser. These
         tests are known to be slower; however, since they test your application at the same layer as your end users,
         they can provide a lot of confidence that your app is working correctly. And, since these tests are run in the
