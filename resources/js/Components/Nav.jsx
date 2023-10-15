e00dcf7e438011b8e6eae79894f027e946e26628
Sharif <54396379+developersharif@users.noreply.github.com>
2023-10-15 10:45:03 +0600

    Update twitter logo

--- resources/js/Components/Nav.jsx
+++ resources/js/Components/Nav.jsx
@@ -19,9 +19,9 @@ const Nav = ({ className }) => {
           </svg>
         </a>
         <a className="mr-5 flex items-center text-gray-500 hover:text-gray-700" href="https://twitter.com/inertiajs">
-          <svg className="h-5 w-5 fill-current" viewBox="0 0 20 20">
+          <svg className="h-5 w-5 fill-current" viewBox="0 0 512 512">
             <title>Twitter</title>
-            <path d="M6.29 18.25c7.55 0 11.67-6.25 11.67-11.67v-.53c.8-.59 1.49-1.3 2.04-2.13-.75.33-1.54.55-2.36.65a4.12 4.12 0 0 0 1.8-2.27c-.8.48-1.68.81-2.6 1a4.1 4.1 0 0 0-7 3.74 11.65 11.65 0 0 1-8.45-4.3 4.1 4.1 0 0 0 1.27 5.49C2.01 8.2 1.37 8.03.8 7.7v.05a4.1 4.1 0 0 0 3.3 4.03 4.1 4.1 0 0 1-1.86.07 4.1 4.1 0 0 0 3.83 2.85A8.23 8.23 0 0 1 0 16.4a11.62 11.62 0 0 0 6.29 1.84" />
+            <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
           </svg>
         </a>
         <a className="flex items-center text-gray-500 hover:text-gray-700" href="https://discord.gg/gwgxN8Y">
