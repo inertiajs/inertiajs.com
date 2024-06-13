f212912cbdac19e17feb1b82c92443147ac01971
Jonathan Reinink <jonathan@reinink.ca>
2024-06-12 22:25:07 -0400

    Improve partial reload docs

--- resources/js/Pages/partial-reloads.jsx
+++ resources/js/Pages/partial-reloads.jsx
@@ -5,7 +5,10 @@ export const meta = {
   title: 'Partial reloads',
   links: [
     { url: '#top', name: 'Introduction' },
-    { url: '#making-partial-visits', name: 'Making partial visits' },
+    { url: '#only-certain-props', name: 'Only certain props' },
+    { url: '#except-certain-props', name: 'Except certain props' },
+    { url: '#router-shorthand', name: 'Router shorthand' },
+    { url: '#using-links', name: 'Using links' },
     { url: '#lazy-data-evaluation', name: 'Lazy data evaluation' },
   ],
 }
@@ -28,10 +31,10 @@ export default function () {
         with the data it already has in memory client-side.
       </P>
       <Notice>Partial reloads only work for visits made to the same page component.</Notice>
-      <H2>Making partial visits</H2>
+      <H2>Only certain props</H2>
       <P>
-        To perform a partial reload, use the <Code>only</Code> property to specify which data the server should return.
-        This option should be an array of keys which correspond to the keys of the props.
+        To perform a partial reload, use the <Code>only</Code> visit option to specify which data the server should
+        return. This option should be an array of keys which correspond to the keys of the props.
       </P>
       <TabbedCode
         examples={[
@@ -81,6 +84,61 @@ export default function () {
           },
         ]}
       />
+      <H2>Except certain props</H2>
+      <P>
+        In addition to the <Code>only</Code> visit option you can also use the <Code>except</Code> option to specify
+        which data the server should exclude. This option should also be an array of keys which correspond to the keys
+        of the props.
+      </P>
+      <TabbedCode
+        examples={[
+          {
+            name: 'Vue 2',
+            language: 'js',
+            code: dedent`
+              import { router } from '@inertiajs/vue2'
+
+              router.visit(url, {
+                except: ['users'],
+              })
+            `,
+          },
+          {
+            name: 'Vue 3',
+            language: 'js',
+            code: dedent`
+              import { router } from '@inertiajs/vue3'
+
+              router.visit(url, {
+                except: ['users'],
+              })
+            `,
+          },
+          {
+            name: 'React',
+            language: 'js',
+            code: dedent`
+              import { router } from '@inertiajs/react'
+
+              router.visit(url, {
+                except: ['users'],
+              })
+            `,
+          },
+          {
+            name: 'Svelte',
+            language: 'js',
+            code: dedent`
+              import { router } from '@inertiajs/svelte'
+
+              router.visit(url, {
+                except: ['users'],
+              })
+            `,
+          },
+        ]}
+      />
+      <H2>Router shorthand</H2>
       <P>
         Since partial reloads can only be made to the same page component the user is already on, it almost always makes
         sense to just use the <Code>router.reload()</Code> method, which automatically uses the current URL.
@@ -125,6 +183,7 @@ export default function () {
           },
         ]}
       />
+      <H2>Using links</H2>
       <P>
         It's also possible to perform partial reloads with Inertia links using the <Code>only</Code> property.
       </P>
@@ -176,9 +235,27 @@ export default function () {
         your server-side routes or controllers. This can be accomplished by wrapping all optional page data in a
         closure.
       </P>
+      <TabbedCode
+        examples={[
+          {
+            name: 'Laravel',
+            language: 'php',
+            code: dedent`
+              return Inertia::render('Users/Index', [
+                  'users' => fn () => User::all(),
+                  'companies' => fn () => Company::all(),
+              ]);
+            `,
+          },
+        ]}
+      />
       <P>
         When Inertia performs a request, it will determine which data is required and only then will it evaluate the
         closure. This can significantly increase the performance of pages that contain a lot of optional data.
+      </P>
+      <P>
+        Additionally, Inertia provides an <Code>Inertia::lazy()</Code> method to specify that a prop should never be
+        included unless explicity requested using the <Code>only</Code> option:
       </P>
       <TabbedCode
         examples={[
@@ -187,20 +264,56 @@ export default function () {
             language: 'php',
             code: dedent`
               return Inertia::render('Users/Index', [
-                  // ALWAYS included on first visit...
+                  'users' => Inertia::lazy(fn () => User::all()),
+              ]);
+            `,
+          },
+        ]}
+      />
+      <P>
+        On the inverse, you can use the <Code>Inertia::always()</Code> method to specify that a prop should always be
+        included, even if it has not been explicity required in a partial reload.
+      </P>
+      <TabbedCode
+        examples={[
+          {
+            name: 'Laravel',
+            language: 'php',
+            code: dedent`
+              return Inertia::render('Users/Index', [
+                  'users' => Inertia::always(User::all()),
+              ]);
+            `,
+          },
+        ]}
+      />
+      <P>Here's a summary of each approach:</P>
+      <TabbedCode
+        examples={[
+          {
+            name: 'Laravel',
+            language: 'php',
+            code: dedent`
+              return Inertia::render('Users/Index', [
+                  // ALWAYS included with standard visits...
                   // OPTIONALLY included on partial reloads...
                   // ALWAYS evaluated...
-                  'users' => User::get(),
+                  'users' => User::all(),
 
-                  // ALWAYS included on first visit...
+                  // ALWAYS included with standard visits...
                   // OPTIONALLY included on partial reloads...
                   // ONLY evaluated when needed...
-                  'users' => fn () => User::get(),
+                  'users' => fn () => User::all(),
 
-                  // NEVER included on first visit...
+                  // NEVER included with standard visits...
                   // OPTIONALLY included on partial reloads...
                   // ONLY evaluated when needed...
-                  'users' => Inertia::lazy(fn () => User::get()),
+                  'users' => Inertia::lazy(fn () => User::all()),
+
+                  // ALWAYS included with standard visits...
+                  // ALWAYS included on partial reloads...
+                  // ALWAYS evaluated...
+                  'users' => Inertia::always(User::all()),
               ]);
             `,
           },
