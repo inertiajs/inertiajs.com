f46b70494ca9aad6cd2cd27a0f5a9a57e9ed54b9
Antoine Auger <128502455+Archidoc142@users.noreply.github.com>
2024-10-18 09:37:13 -0400

    Update error-handling.jsx
    Render "Error" while the component name is "ErrorPage"

--- resources/js/Pages/error-handling.jsx
+++ resources/js/Pages/error-handling.jsx
@@ -57,7 +57,7 @@ export default function () {
               ->withExceptions(function (Exceptions $exceptions) {
                   $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
                       if (! app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
-                          return Inertia::render('Error', ['status' => $response->getStatusCode()])
+                          return Inertia::render('ErrorPage', ['status' => $response->getStatusCode()])
                               ->toResponse($request)
                               ->setStatusCode($response->getStatusCode());
                       } elseif ($response->getStatusCode() === 419) {
