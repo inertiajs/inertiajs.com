541cf4c50cdaa2761e23a3f784038c174c39c614
Pascal Baljet <pascal@pascalbaljet.nl>
2025-06-27 08:42:39 +0200

    Remove `maximum-scale`

--- resources/js/Pages/server-side-setup.jsx
+++ resources/js/Pages/server-side-setup.jsx
@@ -62,7 +62,7 @@ export default function () {
               <html>
                 <head>
                   <meta charset="utf-8" />
-                  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+                  <meta name="viewport" content="width=device-width, initial-scale=1">
                   @vite('resources/js/app.js')
                   @inertiaHead
                 </head>
